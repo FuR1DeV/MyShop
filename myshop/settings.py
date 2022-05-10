441e046d3756ce4945bd66dd8803e43b76b79d3a
Vasili Turtugeshev <vasia19rus@bk.ru>
2022-05-10 22:20:37 +0700

    added url namespaces

--- myshop/settings.py
+++ myshop/settings.py
@@ -116,3 +116,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+# MEDIA_ROOT должен быть без запятой, иначе это будет считаться tuple, а нужен str
+MEDIA_ROOT = (
+    BASE_DIR / 'media'
+)
