5cc7a4af426a7c2cdeff99fe6af8676fcff62937
Vasili Turtugeshev <vasia19rus@bk.ru>
2022-05-10 20:27:39 +0700

    created new tables for DB

--- products/views.py
+++ products/views.py
@@ -1,4 +1,9 @@
+import os
+import json
+
 from django.shortcuts import render
+
+module_dir = os.path.dirname(__file__)
 
 
 def index(request):
@@ -7,5 +12,8 @@ def index(request):
 
 
 def products(request):
-    context = {'title': 'MyShop - Products'}
+    file_path = os.path.join(module_dir, 'fixtures/products.json')
+    prod = json.load(open(file_path, encoding='utf-8'))
+    context = {'title': 'MyShop - Products',
+               'products': prod}
     return render(request, 'products/products.html', context)
