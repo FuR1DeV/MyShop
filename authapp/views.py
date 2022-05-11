c0c3f8a618f9ff13246c6d0305bd487a741d2ce3
Vasili Turtugeshev <vasia19rus@bk.ru>
2022-05-11 11:41:21 +0700

    added form and controller for register

--- authapp/views.py
+++ authapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from authapp.forms import UserLoginForm
+from authapp.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
@@ -19,3 +19,16 @@ def login(request):
         form = UserLoginForm()
     context = {'title': 'MyShop - Authorization', 'form': form}
     return render(request, 'authapp/login.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegisterForm()
+    context = {'title': 'MyShop - Registration', 'form': form}
+    print(context)
+    return render(request, 'authapp/register.html', context)
